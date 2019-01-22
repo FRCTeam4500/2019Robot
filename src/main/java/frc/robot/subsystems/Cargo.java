8aec5f0d3ab34b305611612980a3c0e3602c03db
unknown <lqian11@claytonschools.net>
2019-01-22 17:15:31 -0600

    added the initiator or whatever it is called

--- src/main/java/frc/robot/subsystems/Cargo.java
+++ src/main/java/frc/robot/subsystems/Cargo.java
@@ -2,13 +2,19 @@ package main.java.frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.Talon;
 import edu.wpi.first.wpilibj.command.Subsystem;
-
+import org.usfirst.frc.team4500.robot.RobotMap;
 
 public class Cargo extends Subsystem {
      
     private TalonSRX leftGrab;
     private TalonSRX rightGrab;
     private TalonSRX topGrab;
+
+    public Cargo() {
+            leftGrab = new TalonSRX(RobotMap.LEFTGRAB)
+            rightGrab = new TalonSRX(RobotMap.RIGHTGRAB)
+            topGrab = new TalonSRX(RobotMap.TOPGRAB)
+    }
     
 }
 
