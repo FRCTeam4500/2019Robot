6c93567706b05b5f0b52970d8ba87b4fcb9fc475
unknown <lqian11@claytonschools.net>
2019-01-25 15:49:10 -0600

    added angleGrab

--- src/main/java/frc/robot/subsystems/Cargo.java
+++ src/main/java/frc/robot/subsystems/Cargo.java
@@ -9,11 +9,13 @@ public class Cargo extends Subsystem {
     private TalonSRX leftGrab;
     private TalonSRX rightGrab;
     private TalonSRX topGrab;
+    private TalonSRX angleGrab;
 
     public Cargo() {
             leftGrab = new TalonSRX(RobotMap.LEFTGRAB);
             rightGrab = new TalonSRX(RobotMap.RIGHTGRAB);
             topGrab = new TalonSRX(RobotMap.TOPGRAB);
+            angleGrab = new TalonSRX(RobotMap.ANGLEGRAB);
     }
     
 }
