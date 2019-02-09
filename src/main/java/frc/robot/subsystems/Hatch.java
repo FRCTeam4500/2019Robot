c82e063a67d23598ea2b54f8e7b32bcc48b4cb19
Nicolas Newman <nnewman316@gmail.com>
2019-02-09 10:38:54 -0600

    reformated code

--- src/main/java/frc/robot/subsystems/Hatch.java
+++ src/main/java/frc/robot/subsystems/Hatch.java
@@ -6,7 +6,6 @@
 /*----------------------------------------------------------------------------*/
 
 package frc.robot.subsystems;
-
 import edu.wpi.first.wpilibj.DoubleSolenoid;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
@@ -17,50 +16,53 @@ import frc.robot.RobotMap;
 * Add your docs here.
 */
 public class Hatch extends Subsystem {
-  // Put methods for controlling this subsystem
-  // here. Call these from Commands.
-  
-  private DoubleSolenoid left, right, center;
-  public Hatch(){
-    left = new DoubleSolenoid(RobotMap.LEFT_HATCH_SOLENOID_FORWARD_CHANNEL, RobotMap.LEFT_HATCH_SOLENOID_REVERSE_CHANNEL);
-    right = new DoubleSolenoid(RobotMap.RIGHT_HATCH_SOLENOID_FORWARD_CHANNEL,RobotMap.RIGHT_HATCH_SOLENOID_REVERSE_CHANNEL);
-    center = new DoubleSolenoid(RobotMap.MIDDLE_HATCH_SOLENOID_FORWARD_CHANNEL, RobotMap.MIDDLE_HATCH_SOLENOID_REVERSE_CHANNEL);
-  }
-  @Override
-  public void initDefaultCommand() {
-    // Set the default command for a subsystem here.
-    // setDefaultCommand(new MySpecialCommand());
-  }
-  public void extendCenter(){
-    center.set(Value.kForward);
-  }
-  public void retractCenter(){
-    center.set(Value.kReverse);
-  }
-  public void toggleCenter(){
-    if(center.get() == Value.kForward){
-      center.set(Value.kReverse);
-    }else{
-      center.set(Value.kForward);
+    
+    private DoubleSolenoid left, right, center;
+
+    public Hatch(){
+        left = new DoubleSolenoid(RobotMap.LEFT_HATCH_SOLENOID_FORWARD_CHANNEL, RobotMap.LEFT_HATCH_SOLENOID_REVERSE_CHANNEL);
+        right = new DoubleSolenoid(RobotMap.RIGHT_HATCH_SOLENOID_FORWARD_CHANNEL,RobotMap.RIGHT_HATCH_SOLENOID_REVERSE_CHANNEL);
+        center = new DoubleSolenoid(RobotMap.MIDDLE_HATCH_SOLENOID_FORWARD_CHANNEL, RobotMap.MIDDLE_HATCH_SOLENOID_REVERSE_CHANNEL);
     }
-  }
-  
-  public void extendSides(){
-    left.set(Value.kForward);
-    right.set(Value.kForward);
-  }
-  public void retractSides(){
-    left.set(Value.kReverse);
-    right.set(Value.kReverse);
-  }
-  public void toggleSides(){
-    if(left.get() == Value.kForward){
-      left.set(Value.kReverse);
-      right.set(Value.kReverse);
-    }else{
-      left.set(Value.kForward);
-      left.set(Value.kForward);
+    @Override
+    public void initDefaultCommand() {
+        // setDefaultCommand(new MySpecialCommand());
     }
-  }
-  
+
+    public void extendCenter(){
+        center.set(Value.kForward);
+    }
+
+    public void retractCenter(){
+        center.set(Value.kReverse);
+    }
+
+    public void toggleCenter(){
+        if(center.get() == Value.kForward){
+            center.set(Value.kReverse);
+        } else {
+            center.set(Value.kForward);
+        }
+    }
+    
+    public void extendSides(){
+        left.set(Value.kForward);
+        right.set(Value.kForward);
+    }
+
+    public void retractSides(){
+        left.set(Value.kReverse);
+        right.set(Value.kReverse);
+    }
+
+    public void toggleSides(){
+        if(left.get() == Value.kForward){
+            left.set(Value.kReverse);
+            right.set(Value.kReverse);
+        } else {
+            left.set(Value.kForward);
+            left.set(Value.kForward);
+        }
+    }
+    
 }
