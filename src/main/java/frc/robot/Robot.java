f3279cabbba6b8c47efe6514fb7906780d916f16
Sonakshi Jindal <sjindal23@claytonschools.stl>
2019-02-09 11:40:02 -0600

    Make a CameraInstance class

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -17,6 +17,7 @@ import frc.robot.commands.Robot_Group_PreConfigure;
 import frc.robot.subsystems.Elevator;
 import frc.robot.subsystems.Swerve;
 import frc.robot.subsystems.WheelModule;
+import frc.robot.utility.CameraInstance;
 import frc.robot.utility.automation.Vision;
 
 /**
@@ -33,6 +34,7 @@ public class Robot extends TimedRobot {
     public static Elevator elevator;
     
     public static Vision vision;
+    public static CameraInstance camOne;
     public static OI oi;
 
     public static Preferences pref;
@@ -53,7 +55,8 @@ public class Robot extends TimedRobot {
         elevator = new Elevator();
         
         vision = new Vision();
-
+        camOne = new CameraInstance(CameraInstance.StreamType.SIMPLE,0);
+        camOne.start();
         pidChooser = new SendableChooser<Boolean>();
         pidChooser.setDefaultOption("Disable", false);
         pidChooser.addOption("Enable", true);
---
+++ src/main/java/frc/robot/utility/CameraInstance.java
@@ -0,0 +1,53 @@
+package frc.robot.utility;
+
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
+
+import edu.wpi.cscore.CvSink;
+import edu.wpi.cscore.CvSource;
+import edu.wpi.cscore.UsbCamera;
+import edu.wpi.first.wpilibj.CameraServer;
+
+public class CameraInstance {
+    private StreamType type;
+    private int id;
+    
+    public enum StreamType {
+        SIMPLE, COMPLEX;
+    }
+    public CameraInstance(StreamType type, int id) {
+        this.type = type;
+        this.id = id;
+    }
+    public void start(){
+        if (type == StreamType.SIMPLE) {
+            createSimpleStream();
+        } else if (type == StreamType.COMPLEX) {
+            createComplexStream();
+        }        
+    }
+    private void createSimpleStream(){
+        CameraServer.getInstance().startAutomaticCapture(id);
+
+    }
+    private void createComplexStream(){
+        new Thread(() -> {
+            UsbCamera camera = CameraServer.getInstance().startAutomaticCapture(id);
+            camera.setResolution(640, 480);
+            
+            CvSink cvSink = CameraServer.getInstance().getVideo();
+            CvSource outputStream = CameraServer.getInstance().putVideo("Blur", 640, 480);
+            
+            Mat source = new Mat();
+            Mat output = new Mat();
+            
+            while(!Thread.interrupted()) {
+                cvSink.grabFrame(source);
+                Imgproc.cvtColor(source, output, Imgproc.COLOR_BGR2GRAY);
+                outputStream.putFrame(output);
+            }
+        }).start();
+
+        
+    }
+}
