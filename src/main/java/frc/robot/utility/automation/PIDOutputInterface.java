fee43828004990758d8332effff9039b1e7e9520
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-11 15:49:21 -0500

    Deleted everything lmao

--- src/main/java/frc/robot/utility/automation/PIDOutputInterface.java
+++
@@ -1,7 +0,0 @@
-package frc.robot.utility.automation;
-
-@FunctionalInterface
-@Deprecated
-public interface PIDOutputInterface<T> {
-    public abstract T get(T error, T lastError, T sumOfErrors, T kP, T kI, T kD);
-}
