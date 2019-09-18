ac1be7ff143a23c9ad8287342f4cbbf4758b411e
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-18 18:32:25 -0500

    Added WheelFace class to store used angles

---
+++ src/test/java/frc/robot/swerve/WheelFace.java
@@ -0,0 +1,12 @@
+package frc.robot.swerve;
+
+public class WheelFace {
+    public static final double FORWARD = 0;
+    public static final double FORWARD_LEFT = 1 * Math.PI / 4;
+    public static final double LEFT = 2 * Math.PI / 4;
+    public static final double BACKWARD_LEFT = 3 * Math.PI / 4;
+    public static final double BACKWARD = 4 * Math.PI / 4;
+    public static final double BACKWARD_RIGHT = 5 * Math.PI / 4;
+    public static final double RIGHT = 6 * Math.PI / 4;
+    public static final double FORWARD_RIGHT = 7 * Math.PI / 4;
+}
