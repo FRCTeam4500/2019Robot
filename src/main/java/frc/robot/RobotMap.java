0df36de2c9f0e025bab84c297a70b4b29455eed0
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-20 10:31:22 -0500

    Added some default arm values in RobotMap

---
+++ src/main/java/frc/robot/RobotMap.java
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot;
+
+/**
+ * Add your docs here.
+ */
+public class RobotMap {
+    // TODO: Add actual value for everything
+    public static final int ROTATIONAL_MOTOR = 0;
+    public static final int TIMEOUT = 0;
+    public static final int ARM_MAX = 0;
+    public static final int ARM_MIN = 0;
+}
