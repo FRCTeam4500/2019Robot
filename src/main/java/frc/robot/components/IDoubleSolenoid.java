2be3ff93f6379a4f816be6871cbeef9eccebde2f
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-20 09:45:46 -0500

    Added IDoubleSolenoid component

---
+++ src/main/java/frc/robot/components/IDoubleSolenoid.java
@@ -0,0 +1,27 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.components;
+
+/**
+ * Add your docs here.
+ */
+public interface IDoubleSolenoid {
+    boolean isExtended();
+
+    void extend();
+
+    void retract();
+
+    default void toggleExtension() {
+        if (isExtended()) {
+            retract();
+        } else {
+            extend();
+        }
+    }
+}
