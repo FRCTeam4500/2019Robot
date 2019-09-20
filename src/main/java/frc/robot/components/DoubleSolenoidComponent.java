84b53df5a04bd0f5bb00f8509741bd1d5dda243d
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-20 17:00:25 -0500

    Added DoubleSolenoidComponent

---
+++ src/main/java/frc/robot/components/DoubleSolenoidComponent.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.components;
+
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+
+/**
+ * Add your docs here.
+ */
+public class DoubleSolenoidComponent extends DoubleSolenoid implements IDoubleSolenoid {
+
+    public DoubleSolenoidComponent(int moduleNumber, int forwardChannel, int reverseChannel) {
+        super(moduleNumber, forwardChannel, reverseChannel);
+    }
+
+    public DoubleSolenoidComponent(int forwardChannel, int reverseChannel) {
+        super(forwardChannel, reverseChannel);
+    }
+
+    @Override
+    public boolean isExtended() {
+        if (get() == Value.kForward) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void extend() {
+        set(Value.kForward);
+    }
+
+    @Override
+    public void retract() {
+        set(Value.kReverse);
+    }
+
+}
