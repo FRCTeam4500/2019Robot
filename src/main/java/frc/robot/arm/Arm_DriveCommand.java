06a720502cd7206c7f48442cd2c57719e6c1258f
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-22 13:52:52 -0500

    Added Arm Commands and OI

---
+++ src/main/java/frc/robot/arm/Arm_DriveCommand.java
@@ -0,0 +1,44 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.arm;
+
+/**
+ * Add your docs here.
+ */
+public class Arm_DriveCommand extends Arm_BaseCommand {
+
+    private IArmOI oi;
+
+    public Arm_DriveCommand(Arm arm, IArmOI oi) {
+        super(arm);
+        this.oi = oi;
+    }
+
+    @Override
+    protected void initialize() {
+    }
+
+    @Override
+    protected void execute() {
+        arm.setAngle(oi.getArmAngle());
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return false;
+    }
+
+    @Override
+    protected void end() {
+    }
+
+    @Override
+    protected void interrupted() {
+    }
+
+}
---
+++ src/main/java/frc/robot/arm/Arm_SetAngleCommand.java
@@ -0,0 +1,43 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.arm;
+
+/**
+ * Add your docs here.
+ */
+public class Arm_SetAngleCommand extends Arm_BaseCommand {
+
+    private double angle;
+
+    public Arm_SetAngleCommand(Arm arm, double angle) {
+        super(arm);
+        this.angle = angle;
+    }
+
+    @Override
+    protected void initialize() {
+        arm.setAngle(angle);
+    }
+
+    @Override
+    protected boolean isFinished() {
+        return true;
+    }
+
+    @Override
+    protected void execute() {
+    }
+
+    @Override
+    protected void end() {
+    }
+
+    @Override
+    protected void interrupted() {
+    }
+}
