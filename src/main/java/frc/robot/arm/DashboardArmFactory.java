b1be3e9d87770915d4f51a989dfa910d03b67ecb
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-22 13:41:56 -0500

    Added DashboardArmFactory

---
+++ src/main/java/frc/robot/arm/DashboardArmFactory.java
@@ -0,0 +1,19 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.arm;
+
+import frc.robot.components.dashboard.DashboardAngleSetterComponent;
+
+/**
+ * Add your docs here.
+ */
+public class DashboardArmFactory {
+    public static Arm MakeAr() {
+        return new Arm(new DashboardAngleSetterComponent("Arm"));
+    }
+}
