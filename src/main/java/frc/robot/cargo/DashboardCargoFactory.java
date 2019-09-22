a563c053dbe55730f07f2736d62d575cece15a75
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-22 12:41:07 -0500

    Added DashboardCargoFactory

---
+++ src/main/java/frc/robot/cargo/DashboardCargoFactory.java
@@ -0,0 +1,21 @@
+/*----------------------------------------------------------------------------*/
+/* Copyright (c) 2018 FIRST. All Rights Reserved. */
+/* Open Source Software - may be modified and shared by FRC teams. The code */
+/* must be accompanied by the FIRST BSD license file in the root directory of */
+/* the project. */
+/*----------------------------------------------------------------------------*/
+
+package frc.robot.cargo;
+
+import frc.robot.components.dashboard.DashboardSpeedSetterComponent;
+
+/**
+ * Add your docs here.
+ */
+public class DashboardCargoFactory {
+    public static Cargo MakeCargo() {
+        return new Cargo(new DashboardSpeedSetterComponent("Cargo Left Grab"),
+                new DashboardSpeedSetterComponent("Cargo Top Grab"),
+                new DashboardSpeedSetterComponent("Cargo Right Grab"));
+    }
+}
