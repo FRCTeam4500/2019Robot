6b5e45828b430b706d2128b4db948ca15b8ad5f6
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-23 21:39:24 -0500

    Added jank sendable functionality to DashboardAngleSetterComponent

--- src/main/java/frc/robot/components/dashboard/DashboardAngleSetterComponent.java
+++ src/main/java/frc/robot/components/dashboard/DashboardAngleSetterComponent.java
@@ -7,6 +7,10 @@
 
 package frc.robot.components.dashboard;
 
+import edu.wpi.first.wpilibj.Sendable;
+import edu.wpi.first.wpilibj.SendableBase;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.components.IAngleSetter;
 
 /**
@@ -14,12 +18,24 @@ import frc.robot.components.IAngleSetter;
  */
 public class DashboardAngleSetterComponent extends DashboardComponent implements IAngleSetter {
 
+    private double angle = 0;
+
     public DashboardAngleSetterComponent(String name) {
         super(name + " " + "Angle");
+        // TODO: Cleanup sendable functionality
+        SmartDashboard.putData(name + " " + "(Gyro)", new SendableBase() {
+
+            @Override
+            public void initSendable(SendableBuilder builder) {
+                builder.setSmartDashboardType("Gyro");
+                builder.addDoubleProperty("Value", () -> -angle * 180 / Math.PI, null);
+            }
+        });
     }
 
     @Override
     public void setAngle(double angle) {
         putNumber(angle);
+        this.angle = angle;
     }
 }
