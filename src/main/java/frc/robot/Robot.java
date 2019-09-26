2e1ee55f7159dd037c847668367d2b868398dfcf
Octillerysnacker <octillerysnacker@gmail.com>
2019-09-25 21:14:38 -0500

    Made Robot.java use VirtualPIDSwerveFactoy

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -18,9 +18,10 @@ import frc.robot.lift.*;
 import frc.robot.swerve.*;
 
 /**
- * The VM is configured to automatically run this class, and to call the functions corresponding to
- * each mode, as described in the TimedRobot documentation. If you change the name of this class or
- * the package after creating this project, you must also update the build.gradle file in the
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the TimedRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the build.gradle file in the
  * project.
  */
 public class Robot extends TimedRobot {
@@ -30,8 +31,8 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * This function is run when the robot is first started up and should be used for any
-     * initialization code.
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
      */
     private Swerve swerve;
     private IMainOI oi;
@@ -45,19 +46,20 @@ public class Robot extends TimedRobot {
     @Override
     public void robotInit() {
 
-        setupVirtualSubsystems();
-        // setupCustomSubsystems();
+        // setupVirtualSubsystems();
+        setupCustomSubsystems();
 
         oi = new XboxOI(swerve, lift, hatch, elevator, compressor, cargo, arm);
     }
 
     /**
-     * This function is called every robot packet, no matter the mode. Use this for items like
-     * diagnostics that you want ran during disabled, autonomous, teleoperated and test.
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like diagnostics that you want ran during disabled, autonomous,
+     * teleoperated and test.
      *
      * <p>
-     * This runs after the mode specific periodic functions, but before LiveWindow and
-     * SmartDashboard integrated updating.
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and SmartDashboard integrated updating.
      */
     @Override
     public void robotPeriodic() {
@@ -65,8 +67,9 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * This function is called once each time the robot enters Disabled mode. You can use it to
-     * reset any subsystem information you want to clear when the robot is disabled.
+     * This function is called once each time the robot enters Disabled mode. You
+     * can use it to reset any subsystem information you want to clear when the
+     * robot is disabled.
      */
     @Override
     public void disabledInit() {
@@ -78,15 +81,16 @@ public class Robot extends TimedRobot {
     }
 
     /**
-     * This autonomous (along with the chooser code above) shows how to select between different
-     * autonomous modes using the dashboard. The sendable chooser code works with the Java
-     * SmartDashboard. If you prefer the LabVIEW Dashboard, remove all of the chooser code and
-     * uncomment the getString code to get the auto name from the text box below the Gyro
+     * This autonomous (along with the chooser code above) shows how to select
+     * between different autonomous modes using the dashboard. The sendable chooser
+     * code works with the Java SmartDashboard. If you prefer the LabVIEW Dashboard,
+     * remove all of the chooser code and uncomment the getString code to get the
+     * auto name from the text box below the Gyro
      *
      * <p>
-     * You can add additional auto modes by adding additional commands to the chooser code above
-     * (like the commented example) or additional comparisons to the switch structure below with
-     * additional strings & commands.
+     * You can add additional auto modes by adding additional commands to the
+     * chooser code above (like the commented example) or additional comparisons to
+     * the switch structure below with additional strings & commands.
      */
     @Override
     public void autonomousInit() {
@@ -123,13 +127,12 @@ public class Robot extends TimedRobot {
 
     }
 
-    private void setupSubsystems(SubsystemType swerveType, SubsystemType liftType,
-            SubsystemType hatchType, SubsystemType elevatorType, SubsystemType compressorType,
-            SubsystemType cargoType, SubsystemType armType) {
+    private void setupSubsystems(SubsystemType swerveType, SubsystemType liftType, SubsystemType hatchType,
+            SubsystemType elevatorType, SubsystemType compressorType, SubsystemType cargoType, SubsystemType armType) {
         if (swerveType == SubsystemType.Hardware) {
             swerve = DefaultSwerveFactory.MakeSwerve();
         } else {
-            swerve = VirtualSwerveFactory.MakeSwerve();
+            swerve = VirtualPIDSwerveFactory.MakeSwerve();
         }
 
         if (liftType == SubsystemType.Hardware) {
@@ -169,17 +172,16 @@ public class Robot extends TimedRobot {
         }
     }
 
+    @SuppressWarnings("unused")
     private void setupVirtualSubsystems() {
-        setupSubsystems(SubsystemType.Virtual, SubsystemType.Virtual, SubsystemType.Virtual,
-                SubsystemType.Virtual, SubsystemType.Virtual, SubsystemType.Virtual,
-                SubsystemType.Virtual);
+        setupSubsystems(SubsystemType.Virtual, SubsystemType.Virtual, SubsystemType.Virtual, SubsystemType.Virtual,
+                SubsystemType.Virtual, SubsystemType.Virtual, SubsystemType.Virtual);
     }
 
     @SuppressWarnings("unused") // Code is here as a helper method for quick switching
     private void setupHardwareSubsystems() {
-        setupSubsystems(SubsystemType.Hardware, SubsystemType.Hardware, SubsystemType.Hardware,
-                SubsystemType.Hardware, SubsystemType.Hardware, SubsystemType.Hardware,
-                SubsystemType.Hardware);
+        setupSubsystems(SubsystemType.Hardware, SubsystemType.Hardware, SubsystemType.Hardware, SubsystemType.Hardware,
+                SubsystemType.Hardware, SubsystemType.Hardware, SubsystemType.Hardware);
     }
 
     @SuppressWarnings("unused") // Code is here as a helper method for quick switching
