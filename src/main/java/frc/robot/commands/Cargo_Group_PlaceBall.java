a2509173cfa876e4c29fcedf43f333580cdf746d
csesbeaucha <stephenbeauchamp@claytonschools.net>
2019-09-28 11:25:11 -0500

    Uknown commit

--- src/main/java/frc/robot/commands/Cargo_Group_PlaceBall.java
+++ src/main/java/frc/robot/commands/Cargo_Group_PlaceBall.java
@@ -15,7 +15,7 @@ public class Cargo_Group_PlaceBall extends CommandGroup {
    */
   public Cargo_Group_PlaceBall() {
     // Add Commands here:
-    addSequential(new Elevator_SetLevel(14500));
+    addSequential(new Elevator_SetLevel(15500));
     addSequential(new Arm_SetRotation(2000));
 
     // e.g. addSequential(new Command1());
